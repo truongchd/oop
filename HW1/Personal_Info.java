c736dcd878598bc52919d79e890bd362a8f1d16c
Your Name <you@example.com>
2023-03-13 11:11:35 +0700

    Hoan thanh cau B HW1

---
+++ HW1/Personal_Info.java
@@ -0,0 +1,14 @@
+public class Personal_Info {
+
+    public static void main(String[] args) {
+
+        // Prints "Hello, World" in the terminal window.
+        System.out.println("Chu Huu Dang Truong\t22022505\tdangtruongvnu\t22022505@vnu.edu.vn");
+        for (int i = 9; i >= 1; i = i - 1) {
+            System.out.println(i + " bottles of beer on the wall, " + i + " bottles of beer.");
+            System.out.println("Take one down, pass it around,");
+        }
+        System.out.println("No more bottles of beer on the wall.");
+    }
+
+}
