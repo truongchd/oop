069337d766bb916c826f0f688253a7f360935eec
Your Name <you@example.com>
2023-03-24 15:31:51 +0700

    Singleton problem 1

---
+++ TextGraphics.java
@@ -0,0 +1,51 @@
+import java.util.*;
+import java.io.*;
+
+public class TextGraphics {
+    private char[][] screen;
+    private int width;
+    private int height;
+
+    public TextGraphics(int w, int h) {
+        this.screen = new char[w][h];
+        this.width = w;
+        this.height = h;
+        for (int i = 0; i < this.width; i++) {
+            for (int j = 0; j < this.height; j++) {
+                this.screen[i][j] = ' ';
+            }
+        }
+    }
+
+    /*public TextGraphics getInstance() {
+        return this;
+    }*/
+
+    public void drawPoint(char c, int x, int y) {
+        this.screen[x - 1][y - 1] = c;
+    }
+
+    public void fillRectPoint(char c, int x, int y, int w, int h) {
+        for (int i = x - 1; i < x + w; i++) {
+            for (int j = y - 1; j < y + h; j++){
+                this.screen[i][j] = c;
+            }
+        }
+    }
+
+    public void render() {
+        for (int i = 0; i < this.width; i++) {
+            for (int j = 0; j < this.height; j++) {
+                System.out.print(this.screen[i][j]);
+            }
+            System.out.print("\n");
+        }
+    }
+    public static void main (String[] args) {
+        TextGraphics graphics = new TextGraphics();
+        graphics.init(15, 10);
+        graphics.drawPoint('*', 1, 1);
+        graphics.fillRectPoint('o', 5, 5, 3, 2);
+        graphics.render();
+    }
+}
